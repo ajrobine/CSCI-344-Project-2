76be2a047fd9d82b9d6bcdee157c40314a62cdc1
Andrew Robinette <ajrobine@unca.edu>
2012-02-22 16:48:42 -0500

    Eigth commit.  Placed jquery code for script.js into a .click function

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -1,72 +1,62 @@
+$(document).ready(function() {
+
+	main();
+});
+  
+
 
 function main() {
     
-    //alert("hello world!");
-
-    $("#search_term").click(function() {
-    	alert($("#term").val());
-
-
-	});
-
+   $("#searchbutton").click(function() {
+   
     var count = 0;
-
     var object_array = [];
-
     var query = $("#querybox").val();
-
-
-
+    alert(query);
 
     //Creates a spotter and gets it to insert tweets into the DOM
-
+    
     var s = new Spotter("twitter.search",
-    			{q:query, period:120},
-			{buffer:true, bufferTimeout:750}
+			{q: query, period:120},
+    			{buffer:true, bufferTimeout:750}
 			);
 
-        
+    	s.register(function(tweet) {
+		//Adds profile images (tweet.profile_image_url)
 
-    s.register(function(tweet) {
-        
-	//Adds profile images (tweet.profile_image_url)
+		count = count + 1;
+		var color;
+		var profile_image = "<img src='"+tweet.profile_image_url+"' />";
 
-	count = count + 1;
-	var color;
+		if (count%2 === 0) {
+			color = "red";
+		} else {
+			color = "blue";
+		}
 
-	var profile_image = "<img src='"+tweet.profile_image_url+"' />";
+		var object = $("<p class='"+color+"'>"+profile_image+tweet.text+"</p>");
+		object.hide();
+		$("#tweets").prepend(object);
+		object.fadeIn();
+		object.slideDown();
 
-	if (count%2 === 0) {
-	   color = "red";
-	}  else  {
-	     color = "blue";
-	}
+		object_array.push(object);
 
-	var object = $("<p class='"+color+"'>"+profile_image+tweet.text+"</p>");
-	object.hide();
-	$("#content").prepend(object);
-	object.fadeIn();
-	object.slideDown();
-
-	}
-	
-	object_array.push(object);
-
-	if(object_array.length > 5) {
-	  var object_to_remove = object_array.shift();
-	  object_to_remove.fadeOut(200, function() {
-	     object_to_remove.remove();
-	     });
-	  }
+		if (object_array.length > 5) {
+			var object_to_remove = object_array.shift();
+			object_to_remove.fadeOut(200, function() {
+			object_to_remove.remove();
+			});
+		}
 	});
+  
+	s.start();
+  });	
 
-    s.start();
 
-
+   
+}        
+    
+   	
+	 
 	
-
-
-$(document).ready(function() {
-
-main();
-});
